d4a448384fadfa140af195bb1dbe02c62384a8a6
Jagjit Bhatia <jbhati3@uic.edu>
2020-10-14 14:18:50 -0500

    orgService API's to retrieve all orgs and join an org

--- candle-server/org-service/orgService.js
+++ candle-server/org-service/orgService.js
@@ -10,6 +10,22 @@ app.use(bodyParser.json());
 
 const db_url = "mongodb://localhost:27017/";    // TODO: Update url with URL of hosted database
 
+// Get All Orgs API
+app.get('/getAllOrgs', (req, res) => {
+    mongo.connect(db_url, (err, db) => {
+        if(err) throw err;
+
+        let dbo = db.db('candleDB');
+
+        dbo.collection('orgs').find({}).toArray((err, result) => {
+            if(err) throw err;
+
+            res.status(200).send({orgs: result});
+            db.close();
+        })
+    });
+});
+
 // Create Org API
 app.post('/createOrg', (req, res) => {
     mongo.connect(db_url, (err, db) => {
@@ -23,7 +39,9 @@ app.post('/createOrg', (req, res) => {
             logo_url: req.body.logo_url,
             bio: req.body.bio,
             institution: req.body.institution,
-            members: [{id: req.body.user_id, role: 'admin', title: req.body.title || ''}]   
+            members: [{id: req.body.user_id, role: 'admin', title: req.body.title || ''}],
+            requests: [],
+            public: req.body.public       // If this is set to true, then anyone can join
         }
 
         dbo.collection('orgs').insertOne(new_profile, (err, doc) => {
@@ -38,7 +56,10 @@ app.post('/createOrg', (req, res) => {
 // Add member API
 app.put('/addMember', (req, res) => {
     mongo.connect(db_url, (err, db) => {
-        if(err) throw err;
+        if(err) {
+            console.log("this is the prahblem");
+            throw err;
+        }
 
         let dbo = db.db('candleDB');
 
@@ -74,7 +95,7 @@ app.put('/addMember', (req, res) => {
 
                     dbo.collection('orgs').updateOne(filter, query, (err, result) => {
                         if(err) throw err;
-                        res.status(202).send("New member added.");
+                        res.status(202).send(result);
                         db.close();
                     });
                 }
@@ -82,6 +103,39 @@ app.put('/addMember', (req, res) => {
         });
     });
 });
+
+app.put('/joinOrg', (req, res) => {
+    mongo.connect(db_url, (err, db) => {
+        if(err) throw err;
+
+        let dbo = db.db('candleDB');
+
+        dbo.collection('orgs').findOne({_id: new mongodb.ObjectId(req.body.org_id)}, (err, result) => {
+            if(err) throw err;
+
+            if(!result) {
+                res.status(400).send("Organization does not exist.");
+                db.close();
+            }
+
+            if(result.public) {
+                res.redirect(307, '/addMember');
+            }
+
+            else {
+                let filter = {_id: new mongodb.ObjectId(req.body.org_id)};
+                let query = { $push: { requests: {id: req.body.newMember_id, role: req.body.role, title: req.body.title || ''} } };
+
+                dbo.collection('orgs').updateOne(filter, query, (err, result) => {
+                    if(err) throw err;
+                    res.status(202).send(result);
+                    db.close();
+                });
+            }
+
+        });
+    }); 
+}); 
 
 
 app.listen(port);
